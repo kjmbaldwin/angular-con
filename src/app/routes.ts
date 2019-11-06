c76749dce61252f67a8f7dcb86d081bf0fa44bdf
kjmbaldwin <kylejmbaldwin@gmail.com>
2019-11-06 09:45:33 -0500

    spelling is hard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,8 @@
+import { Routes } from "@angular/router";
 import { EventListComponent } from "./events/events-list.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 
-export const appRoute = [
+export const appRoutes:Routes = [
     { path: '', redirectTo: '/events', pathMatch: 'full' },
     { path: 'events', component: EventListComponent },
     { path: 'events/:id', component: EventDetailsComponent }
